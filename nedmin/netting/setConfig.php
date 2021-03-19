a0ff3ebde964ee43fba78042c6615b4426d65c7d
= <alitunacanonar@icloud.com>
2021-03-19 20:00:58 +0300

    git ignore eklendi

--- nedmin/netting/setConfig.php
+++ nedmin/netting/setConfig.php
@@ -1,5 +1,20 @@
 <?php
-if (!isset($_SESSION['admins'])) {
+require_once 'class.crud.php';
+$db = new CRUD();
+
+if (!isset($_SESSION['admins']) && isset ($_COOKIE['adminsLogin'])) {
+    $adminsLogin = json_decode($_COOKIE['adminsLogin']);
+
+    $result = $db->adminsLogin($adminsLogin->admins_username, $adminsLogin->admins_password, TRUE);
+
+    if ($sonuc['status']) {
+        header("Location: index.php");
+        exit;
+    }
+}
+
+
+if (!isset($_SESSION['admins']) && !isset($_COOKIE['adminsLogin'])) {
     header('Location: login.php');
-    exit; 
+    exit;
 }
