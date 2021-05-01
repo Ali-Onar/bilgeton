9933eb4584a270522ef6f40933b9958ff5d0e8ce
= <alitunacanonar@icloud.com>
2021-05-01 14:54:33 +0300

    dbco

--- nedmin/netting/dbConfig.php
+++ nedmin/netting/dbConfig.php
@@ -5,11 +5,6 @@ if ($_SERVER['HTTP_HOST'] == 'localhost') {
     define('DBUSER', 'root');
     define('DBNAME', 'bilgeton');
     define('DBPWD', '');
-} else {
-    define('DBHOST', 'localhost');
-    define('DBUSER', 'hazirli1_bilgetonadmin');
-    define('DBNAME', 'hazirli1_bilgeton');
-    define('DBPWD', 'riozWAS-Fh6U');
 }
 
 
